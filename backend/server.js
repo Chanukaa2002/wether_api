f232d6a813fa3f8ee1f40bd7651c556681a3c156
Chanuka Dilshan <mchanuka72@gmail.com>
2025-02-04 08:05:08 +0530

    adgest cors

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const allowedOrigins = [
+  "https://weather-check-pearl-xi.vercel.app",
+  "http://localhost:5173"
+];
+
 app.use(
   cors({
-    origin: "https://weather-check-pearl-xi.vercel.app/",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
+
 app.use("/api/weather", wetherRoute);
 
 app.listen(PORT, () => {
